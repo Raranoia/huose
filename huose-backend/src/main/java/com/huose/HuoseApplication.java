5d04c1cbbff5e36740d5dda1c651d09316fbb360
Erwin1912 <16656005502@163.com>
2026-02-27 19:50:07 +0800

    Create HuoseApplication.java as the main Spring Boot application entry point

---
+++ huose-backend/src/main/java/com/huose/HuoseApplication.java
@@ -0,0 +1,12 @@
+package com.huose;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HuoseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HuoseApplication.class, args);
+    }
+}
